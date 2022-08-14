be08299a2f430cfdfe57780ea9b5e079f9e25850
Paulo Sérgio SAMPAIO de ARAGÃO <paulo.aragao@gmail.com>
2022-08-14 20:47:40 +0200

    initial commit

---
+++ en/v0.1/apidoc/cesium/Workers/ArcType-f5af12f9.js
@@ -0,0 +1,2 @@
+/*! For license information please see ArcType-f5af12f9.js.LICENSE.txt */
+define(["exports"],(function(e){"use strict";var r=Object.freeze({NONE:0,GEODESIC:1,RHUMB:2});e.ArcType=r}));
---
+++ en/v0.1/apidoc/cesium/Workers/GeometryOffsetAttribute-3e5f3e97.js
@@ -0,0 +1,2 @@
+/*! For license information please see GeometryOffsetAttribute-3e5f3e97.js.LICENSE.txt */
+define(["exports"],(function(e){"use strict";var t=Object.freeze({NONE:0,TOP:1,ALL:2});e.GeometryOffsetAttribute=t}));
